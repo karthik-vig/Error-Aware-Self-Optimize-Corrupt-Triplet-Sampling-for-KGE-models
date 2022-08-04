28ac94062585831ef33ddb2d4132acbca0477ea9
Karthik Vignesh Sundar <2649025s@student.gla.ac.uk>
2022-08-04 04:35:01 +0100

    refactoring in train_and_evaluate.py to split train and evaluation into two classes.

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -2,79 +2,23 @@ import torch
 from torch.utils.data import TensorDataset, DataLoader
 
 
-class TransE_train_evaluate():
-    def __init__(self, train_dataset, val_dataset, batch_size, num_entity, model, device, optimizer, epoch, seed):
+class TransETrain():
+    def __init__(self, train_dataset, batch_size, num_entity, model, device, optimizer, epoch, seed):
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         self.train_dataset = train_dataset
-        self.val_dataset = TensorDataset(val_dataset)
         self.batch_size = batch_size
         self.num_entity = num_entity
         self.model = model
         self.device = device
         self.optimizer = optimizer
         self.epoch = epoch
-
-        self.val_dataset_len = len(self.val_dataset)
         self.train_dataset_len = self.train_dataset.shape[0]
         self.train_data_loader = self.create_dataloader(self.train_dataset)
-
 
     def create_dataloader(self, dataset):
         tensor_dataset = TensorDataset(dataset)
         return DataLoader(tensor_dataset, batch_size=self.batch_size, shuffle=False, pin_memory=True)
-
-
-    def evaluate_model(self):
-        print('Starting Evaluation:')
-        score_tensor = torch.zeros(self.val_dataset_len * 2, dtype=torch.float64).to(self.device, non_blocking=True)
-        epoch = 0
-        rank_count = 0
-        test_triplet = torch.zeros(self.num_entity, 3, dtype=torch.int64)
-        entity_tensor = torch.arange(0, self.num_entity, dtype=torch.int64)
-        for triplet in self.val_dataset:
-            #calculate the tail rank:
-            original_tail = triplet[0][2]
-            test_triplet[:, 0] = triplet[0][0]
-            test_triplet[:, 1] = triplet[0][1]
-            test_triplet[:, 2] = entity_tensor
-            test_triplet = test_triplet.to(self.device, non_blocking=True)
-            sorted_score_index = self.model.predict(test_triplet)
-            # tail_rank = (sorted_score_index == original_tail).nonzero()[0, 0]
-            tail_rank = torch.where(sorted_score_index == original_tail)[0]
-            del sorted_score_index
-            tail_rank += torch.tensor(1)
-
-            #calculate the head rank:
-            original_head = triplet[0][0]
-            test_triplet[:, 0] = entity_tensor
-            test_triplet[:, 1] = triplet[0][1]
-            test_triplet[:, 2] = triplet[0][2]
-            test_triplet = test_triplet.to(self.device, non_blocking=True)
-            sorted_score_index = self.model.predict(test_triplet)
-            # head_rank = (sorted_score_index == original_head).nonzero()[0, 0]
-            head_rank = torch.where(sorted_score_index == original_head)[0]
-            del sorted_score_index
-            head_rank += torch.tensor(1)
-
-            # Add rank values to a tensor:
-            score_tensor[rank_count] = tail_rank
-            score_tensor[rank_count + 1] = head_rank
-            rank_count += 2
-            epoch += 1
-            if epoch % 5000 == 0:
-                print('Triplets evaluated: ', epoch)
-        mr_score = torch.mean(score_tensor)
-        mrr_score = torch.reciprocal(score_tensor).mean()
-        hit_at_10_score = torch.where(score_tensor < 11.0, 1.0, 0.0).mean()
-
-        print('Mean Rank for prediction is: ', mr_score)
-        print('Mean Reciprocal Rank for prediction is: ', mrr_score)
-        print('Hits@10 for prediction is: ', hit_at_10_score)
-
-        return mr_score, mrr_score, hit_at_10_score
-
-
 
     def create_corr_triplet(self, sample_data):
         corr_triplet = sample_data.clone().detach()
@@ -85,8 +29,6 @@ class TransE_train_evaluate():
         else:
             corr_triplet[:, 2] = entity_tensor
         return corr_triplet
-
-
 
     def train_transe(self):
         print('Starting Training:')
@@ -104,3 +46,56 @@ class TransE_train_evaluate():
             print(i, 'epoch is done')
             print('Average Training loss is: ', avg_train_loss / self.train_dataset_len)
             # evaluate_model()
+
+
+
+class Evaluation():
+    def __init__(self, data, model, num_entity, device):
+        self.dataset = data
+        self.model = model
+        self.num_entity = num_entity
+        self.device = device
+        self.all_entities = self.all_entities = torch.arange(0, self.num_entity, dtype=torch.int64).to(self.device, non_blocking=True)
+        self.test_triplet = torch.zeros(self.num_entity, 3, dtype=torch.int64).to(self.device, non_blocking=True)
+
+    def get_ranking_list(self, all_head, triplet):
+        self.test_triplet[:, 1]=triplet[1]
+        compare_val = None
+        if all_head:
+            compare_val=triplet[0]
+            self.test_triplet[:, 0]=self.all_entities
+            self.test_triplet[:, 2]=triplet[2]
+        else:
+            compare_val=triplet[2]
+            self.test_triplet[:, 0]=triplet[0]
+            self.test_triplet[:, 2]=self.all_entities
+        ranked_entities = self.model.predict(self.test_triplet)
+        pos = torch.where(ranked_entities == compare_val)[0]
+        del ranked_entities
+        rank = pos + 1
+        return rank
+
+    def evaluate_model(self):
+        print('Starting Evaluation:')
+        tail_score_tensor = torch.zeros(self.dataset.shape[0], dtype=torch.float64).to(self.device, non_blocking=True)
+        head_score_tensor = torch.zeros(self.dataset.shape[0], dtype=torch.float64).to(self.device, non_blocking=True)
+        for index, triplet in enumerate(self.dataset):
+            # calculate the tail rank:
+            tail_rank = self.get_ranking_list(all_head=False, triplet=triplet.to(device=self.device, non_blocking=True))
+            tail_score_tensor[index] = tail_rank
+            # calculate the head rank:
+            head_rank = self.get_ranking_list(all_head=True, triplet=triplet.to(device=self.device, non_blocking=True))
+            head_score_tensor[index] = head_rank
+            # to show progress
+            if index % 10000:
+                print('Triplets evaluated: ', index)
+        # concat the two tesors, to produce a final listing of ranks
+        score_tensor = torch.cat((tail_score_tensor, head_score_tensor))
+        mr_score = torch.mean(score_tensor)
+        mrr_score = torch.reciprocal(score_tensor).mean()
+        hit_at_10_score = torch.where(score_tensor < 11.0, 1.0, 0.0).mean()
+        print('Mean Rank for prediction is: ', mr_score)
+        print('Mean Reciprocal Rank for prediction is: ', mrr_score)
+        print('Hits@10 for prediction is: ', hit_at_10_score)
+        return mr_score, mrr_score, hit_at_10_score
+
